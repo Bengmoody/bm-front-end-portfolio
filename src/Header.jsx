4310479cfe0ac56c1c35edb380eb6614218b5fbf
Bengmoody <bengmoody@hotmail.co.uk>
2023-01-09 14:06:01 +0000

    plan uploaded as screenshots

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <div className="header">
+            <div className="title-box">
+                <h1>BM Portfolio Website</h1>
+            </div>
+        </div>
+
+    )
+
+
+}
+
+export default Header
