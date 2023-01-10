d4cb888e2e013065f9b079f55fbf70fc53e5f584
Bengmoody <bengmoody@hotmail.co.uk>
2023-01-10 16:10:21 +0000

    working solution for 7-patch review votes

--- src/AddReviewLikes.jsx
+++ src/AddReviewLikes.jsx
@@ -1,10 +1,35 @@
 import { Link } from "react-router-dom";
-function AddReviewLikes() {
+import { patchSingleReview } from "./api";
+
+function AddReviewLikes({likes,setSingleReview,review_id}) {
+    const handleOnClick = (e) => {
+        let increment = 0;
+        if (e.target.innerText === "👍") {
+            increment += 1;
+        } else {
+            increment -= 1;
+        }
+        setSingleReview((currRev) => {
+            let newRev = {...currRev}
+            newRev.votes += increment;
+            return newRev;
+        })
+        patchSingleReview(review_id,increment)
+        .catch((err) => {
+            setSingleReview((currRev) => {
+                let newRev = {...currRev}
+                newRev.votes -= increment;
+                return newRev;
+            })
+        })
+
+    }
     return (
         <div className="add-review-likes">
             <div className="add-review-likes__container">
-                <Link style={{textDecoration: 'none'}}><div className="add-review-likes__button">👍</div></Link>
-                <Link style={{textDecoration: 'none'}}><div className="add-review-likes__button">👎</div></Link>
+                <button type="button" onClick={handleOnClick} id="up"><div className="add-review-likes__button">👍</div></button>
+                <p>{likes}</p>
+                <button type="button" onClick={handleOnClick} id="down"><div className="add-review-likes__button">👎</div></button>
             </div>
 
         </div>
--- src/api.js
+++ src/api.js
@@ -18,11 +18,20 @@ export const getSingleReview = (review_id) => {
 
 export const getSingleReviewComments = (review_id) => {
     return myApi.get(`/reviews/${review_id}/comments`).then((res) => {
-        console.log(res)
         if (res.data.comments === undefined) {
             return Promise.reject();
         } else {
            return res.data.comments 
+        }
+    })
+}
+
+export const patchSingleReview = (review_id,increment) => {
+    let patchBody={"inc_votes":increment}
+    return myApi.patch(`/reviews/${review_id}`,patchBody)
+    .then((res) => {
+        if (res.status !== 202) {
+            return Promise.reject();
         }
     })
 }
